bce6423440592f7b942dfc4cacbb9ca35b8d4ad6
Vincent1976 <jiangxinmin@163.com>
2021-11-10 13:33:55 +0800

    修改bug

---
+++ pp/Controllers/PlanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pp.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET: Plan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
